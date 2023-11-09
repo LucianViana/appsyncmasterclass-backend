b32e391a30cb7b311596794837d91bf10a48dcb5
LucianViana <luciano.souzaviana@gmail.com>
2023-11-09 14:02:31 -0300

    erro admin cognito

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -7,6 +7,7 @@ const velocityUtil = require('amplify-appsync-simulator/lib/velocity/util')
 
 const { RELATIONSHIPS_TABLE } = process.env
 const when = require('./when');
+const then = require('./then');
 
 const a_random_user = () => {
   const firstName = chance.first({ nationality: 'en' })
@@ -99,33 +100,11 @@ const an_authenticated_user = async () => {
             })
             .promise()
 
-      console.log(challengeResp + `incluir - user has signed up [${username}]`)
+      console.log( challengeResp + `challengeResp - user has signed up [${username}]`)
 
-            // it("should save in DB the user's profile", async () => {
-            //   const { name, email } = given.a_random_user();
-            //   const username = chance.guid();
-        
-      insertUsersTable =  await when.we_invoke_confirmUserSignup(username, name, email);
-      
-      console.log(insertUsersTable + `incluir - user has signed up [${username}]`)
-              // const ddbUser = await then.user_exists_in_UsersTable(username);
-        
-              // expect(ddbUser).toMatchObject({
-              //   id: username,
-              //   name,
-              //   createdAt: expect.stringMatching(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?Z?/g),
-              //   followersCount: 0,
-              //   followingCount: 0,
-              //   tweetsCount: 0
-              //   //TODO : NAO FUNCIONA COM 1 E COM ZERO OLHAR.
-              //   //likesCount: 0
-              // });
-        
-              // const [firstName, lastName] = name.split(' ');
-              // expect(ddbUser.screenName).toContain(firstName);
-              // expect(ddbUser.screenName).toContain(lastName);
-            // })
-
+      const insertUsersTable =  await when.we_invoke_confirmUserSignup(username, name, email);
+      const ddbUser = await then.user_exists_in_UsersTable(username);
+      console.log( ddbUser + `insertUsersTable - user has signed up [${username}]`)
             
     return {
       username,
